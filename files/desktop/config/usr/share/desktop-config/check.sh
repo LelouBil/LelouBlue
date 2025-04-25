d6d3f6b245c90e178b6ada1943c856049348612b
LelouBil <bilel.medimegh@gmail.com>
2025-04-25 17:56:51 -0400

    fix env

--- files/desktop/config/usr/share/desktop-config/check.sh
+++ files/desktop/config/usr/share/desktop-config/check.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+source /usr/lib/environment.d/desktop-env.conf
 export XDG_RUNTIME_DIR=/tmp
 exec Hyprland --verify-config --i-am-really-stupid --config "/usr/share/desktop-config/hyprland/hyprland.conf"
 exit 1
