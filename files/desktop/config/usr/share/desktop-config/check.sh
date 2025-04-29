c363351ad3b022a7ba7ffd120d67915fdb4689de
LelouBil <bilel.medimegh@gmail.com>
2025-04-29 11:19:07 -0400

    fix stuff

--- files/desktop/config/usr/share/desktop-config/check.sh
+++ files/desktop/config/usr/share/desktop-config/check.sh
@@ -3,5 +3,5 @@ set -a
 . /usr/lib/environment.d/desktop-env.conf
 set +a
 export XDG_RUNTIME_DIR=/tmp
-exec Hyprland --verify-config --i-am-really-stupid --config "$HYPRLAND_CONFIG_DIR/hyprland.conf"
+exec Hyprland --verify-config --i-am-really-stupid
 exit 1
