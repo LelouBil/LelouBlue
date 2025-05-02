302ade8eda124623bf19aaf0f81a7e282d25e5f1
LelouBil <bilel.medimegh@gmail.com>
2025-05-02 00:57:33 -0400

    fix

--- files/desktop/config/usr/share/desktop-config/hyprland/scripts/logout.sh
+++ files/desktop/config/usr/share/desktop-config/hyprland/scripts/logout.sh
@@ -3,4 +3,4 @@
 # required for graceful exit since many apps aren't good SIGNAL citizens
 HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
 hyprctl --batch "$HYPRCMDS" >> /tmp/hypr/hyprexitwithgrace.log 2>&1
-hyprctl dispatch exit
+loginctl kill-session ""
