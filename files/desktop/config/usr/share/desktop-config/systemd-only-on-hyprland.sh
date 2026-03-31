0beb39e458569991c0f73eaa10ba6a32c325fc85
LelouBil <bilel.medimegh@gmail.com>
2026-03-31 10:53:13 +0200

    fix script and todo

--- files/desktop/config/usr/share/desktop-config/systemd-only-on-hyprland.sh
+++ files/desktop/config/usr/share/desktop-config/systemd-only-on-hyprland.sh
@@ -16,6 +16,6 @@ UNITS=(
 for unit in "${UNITS[@]}"; do
     unit_dir="/usr/lib/systemd/user/${unit}.d"
     mkdir -p "$unit_dir"
-    ln -sf "$DESKTOP_CONFIG_DIR/systemd/only-on-hyprland.conf" "$unit_dir/only-on-hyprland.conf"
+    ln -sf "$DESKTOP_CONFIG_DIR/systemd-only-on-hyprland.conf" "$unit_dir/only-on-hyprland.conf"
 done
 
