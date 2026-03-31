1f4b53ba296bff796c65120a7acd99148bb0f8aa
LelouBil <bilel.medimegh@gmail.com>
2026-03-31 10:25:52 +0200

    Fix services only on hyprland

---
+++ files/desktop/config/usr/share/desktop-config/only-on-hyprland.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euxo pipefail
+set -a
+. /usr/lib/environment.d/desktop-env.conf
+set +a
+
+# List of systemd user units to configure
+UNITS=(
+    "hyprpolkitagent.service"
+    "hypridle.service"
+    "hyprsunset.service"
+    "dunst.service"
+    "waybar.service"
+)
+
+for unit in "${UNITS[@]}"; do
+    unit_dir="/usr/lib/systemd/user/${unit}.d"
+    mkdir -p "$unit_dir"
+    ln -sf "$DESKTOP_CONFIG_DIR/systemd/only-on-hyprland.conf" "$unit_dir/only-on-hyprland.conf"
+done
+
